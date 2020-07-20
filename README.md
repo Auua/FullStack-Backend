c4d3089bf1f071442c3c83d97de0e10d51f98288
Anna Luhtakanta <dev.luhtakanta@outlook.com>
2020-07-20 18:09:52 +0300

    Add dockerfile

--- README.md
+++ README.md
@@ -1,3 +1,23 @@
 # FullStack-Backend
 
+Working example
 http://fullstack-puhelin.herokuapp.com/
+
+
+### Before building Docker
+
+To connect MongoDB, add your url to MONGODB_URI in Dockerfile
+
+
+```bash
+docker build -t _NAME_ .
+docker run -p 3001:3001 _NAME_
+```
+
+
+### Locally start application
+
+Add MongoDB url to .env file
+MONGODB_URI=_YOUR_URL_HERE_
+
+`npm start` opens server to localhost:3001
--- index.js
+++ index.js
@@ -2,16 +2,17 @@ const express = require('express'),
   bodyParser = require('body-parser'),
   morgan = require('morgan'),
   cors = require('cors'),
-  static = require('static'),
   app = express(),
-  persons= require('./api/mongo')
+  persons = require('./api/mongo')
 
-morgan.token('info', function getInfo (req) {
-  return JSON.stringify({  "name": req.body.name, "number": req.body.number })
+morgan.token('info', function getInfo(req) {
+  return JSON.stringify({ name: req.body.name, number: req.body.number })
 })
 
 app.use(bodyParser.json())
-app.use(morgan(':method :url :status :info :res[content-length] - :response-time ms'))
+app.use(
+  morgan(':method :url :status :info :res[content-length] - :response-time ms')
+)
 app.use(cors())
 app.use(express.static('build'))
 
@@ -22,11 +23,11 @@ app.use(express.static('build'))
 
 app.get('/info', (req, res) => {
   const date = new Date()
-  persons.countAll().then(result =>  {
-    const info = 
-    `<h5>Puhelinluettelossa on ${result} henkilön tiedot</h5>
+  persons.countAll().then((result) => {
+    const info = `<h5>Puhelinluettelossa on ${result} henkilön tiedot</h5>
     <p>${date}</p>`
-    res.send(info)})
+    res.send(info)
+  })
 })
 
 app.get('/api/persons', (req, res) => {
@@ -37,7 +38,7 @@ app.post('/api/persons', (req, res) => {
   const body = req.body
 
   if (body.name === undefined || body.number === undefined) {
-    return res.status(400).json({error: 'content missing'})
+    return res.status(400).json({ error: 'content missing' })
   }
   persons.addPerson(req, res)
 })
@@ -53,7 +54,7 @@ app.delete('/api/persons/:id', (req, res) => {
 app.put('/api/persons/:id', (req, res) => {
   persons.updatePerson(req, res)
 })
-  
+
 const PORT = process.env.PORT || 3001
 app.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`)
